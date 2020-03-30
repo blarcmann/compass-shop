ba0c1907d3813d7683c7eb49b4ea3edb05aa6e00
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-03-31 00:59:14 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/components/footer.scss';
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Foooooootttttteeeeerrrrrrr</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="footer">
+          <div className="menus">
+            <Link to="/home">Home</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/contact">Contact Us</Link>
+          </div>
+          <div className="writer">
+            <a href="https://twitter.com/yemiotola" className="writer" target="_blank" rel="noopener noreferrer">yemiOdetola</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
