a7599fc7150794fc108ca0aae3a5d760a1f8321f
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-03-31 22:55:10 +0100

    mocked home

---
+++ src/constants/mock.js
@@ -0,0 +1,23 @@
+const collections = ['exercise', 'running', 'sneakers', 'sport', 'swim - equipment'];
+const categories = ['featured', 'top', 'women', 'children', 'men'];
+const menuItems = ['Tracksuits', 'Tracksuits', 'Tracksuits', 'Tracksuits'];
+const arrivals = [
+  {
+    bg: 'https://i.ibb.co/WyH2H3n/arrivals-a.jpg',
+    title: 'Summer sales',
+    subtitle: 'packed experience the power of nike',
+  },
+  {
+    bg: 'https://i.ibb.co/q96Pm4V/swimming-1.jpg',
+    title: 'New arrivals',
+    subtitle: 'New kits for the best prices',
+  },
+]
+
+export { menuItems, arrivals, collections, categories }
+
+// https://i.ibb.co/WyH2H3n/arrivals-a.jpg - arrv
+// https://i.ibb.co/q96Pm4V/swimming-1.jpg - arrv
+// https://i.ibb.co/p1dCz86/tenniss.jpg - arrv
+// https://i.ibb.co/DpSYD60/home-banner.jpg - banner
+// https://i.ibb.co/nDPG5YV/ads-banner.jpg - ads
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,8 +4,51 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/screens/home.scss';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { menuItems, arrivals, categories, collections } from '../constants/mock';
 
 export class Home extends Component {
+
+  renderMenu(item, index) {
+    return (
+      <div className="col-md-3 col-sm-6" key={index}>
+        <Link to="/shop" className="menu-cover">
+          <div className="menu"></div>
+          <div className="content">
+            <h2 className="title">{item}</h2>
+            <span className="subtitle">shop now</span>
+          </div>
+        </Link>
+      </div>
+    )
+  }
+
+  renderArrivals(arrv, i) {
+    return (
+      <div className="col-lg-6" key={i}>
+        <div className="arrival" style={{ backgroundImage: `url(${arrv.bg})` }}>
+          <div className="arrival-item">
+            <h2>{arrv.title}</h2>
+            <p>{arrv.subtitle}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderCollections(cat, i, coll) {
+    return (
+      <div className="collection" key={i}>
+        <div className="tile">{cat}</div>
+        <ul>
+          {coll.map(col => (
+            <li><Link to="/shop">{col}</Link></li>
+          ))}
+        </ul>
+      </div>
+
+    )
+  }
+
   render() {
     return (
       <>
@@ -26,62 +69,16 @@ export class Home extends Component {
           <div className="container">
             <div className="main-menu">
               <div className="row">
-                <div className="col-md-3 col-sm-6">
-                  <Link to="/shop" className="menu-cover">
-                    <div className="menu"></div>
-                    <div className="content">
-                      <h2 className="title">Tracksuits</h2>
-                      <span className="subtitle">shop now</span>
-                    </div>
-                  </Link>
-                </div>
-                <div className="col-md-3 col-sm-6">
-                  <Link to="/shop" className="menu-cover">
-                    <div className="menu"></div>
-                    <div className="content">
-                      <h2 className="title">Tracksuits</h2>
-                      <span className="subtitle">shop now</span>
-                    </div>
-                  </Link>
-                </div>
-                <div className="col-md-3 col-sm-6">
-                  <Link to="/shop" className="menu-cover">
-                    <div className="menu"></div>
-                    <div className="content">
-                      <h2 className="title">Tracksuits</h2>
-                      <span className="subtitle">shop now</span>
-                    </div>
-                  </Link>
-                </div>
-                <div className="col-md-3 col-sm-6">
-                  <Link to="/shop" className="menu-cover">
-                    <div className="menu"></div>
-                    <div className="content">
-                      <h2 className="title">Tracksuits</h2>
-                      <span className="subtitle">shop now</span>
-                    </div>
-                  </Link>
-                </div>
+                {menuItems.map((item, index) => (
+                  this.renderMenu(item, index)
+                ))}
               </div>
             </div>
             <div className="arrivals">
               <div className="row">
-                <div className="col-lg-6">
-                  <div className="arrival">
-                    <div className="arrival-item">
-                      <h2>New Arrivals</h2>
-                      <p>shop best swimming equipment</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="arrival">
-                    <div className="arrival-item">
-                      <h2>Summer sales</h2>
-                      <p>experience the power of nike</p>
-                    </div>
-                  </div>
-                </div>
+                {arrivals.map((item, index) => (
+                  this.renderArrivals(item, index)
+                ))}
               </div>
             </div>
             <div className="row">
@@ -95,121 +92,9 @@ export class Home extends Component {
               <div className="categories">
                 <div className="title">2020 Collections</div>
                 <div className="collections">
-                  <div className="collection">
-                    <div className="tile">Featured</div>
-                    <ul>
-                      <li>
-                        <Link to="/shop">exercise</Link>
-                      </li>
-                      <li>
-                        <Link to="shop">running</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sport</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">swim-equipment</Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="collection">
-                    <div className="tile">Top</div>
-                    <ul>
-                      <li>
-                        <Link to="/shop">exercise</Link>
-                      </li>
-                      <li>
-                        <Link to="shop">running</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sport</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">swim-equipment</Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="collection">
-                    <div className="tile">Women</div>
-                    <ul>
-                      <li>
-                        <Link to="/shop">exercise</Link>
-                      </li>
-                      <li>
-                        <Link to="shop">running</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sport</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">swim-equipment</Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="collection">
-                    <div className="tile">Children</div>
-                    <ul>
-                      <li>
-                        <Link to="/shop">exercise</Link>
-                      </li>
-                      <li>
-                        <Link to="shop">running</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sport</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">swim-equipment</Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="collection">
-                    <div className="tile">Men</div>
-                    <ul>
-                      <li>
-                        <Link to="/shop">exercise</Link>
-                      </li>
-                      <li>
-                        <Link to="shop">running</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sneakers</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">sport</Link>
-                      </li>
-                      <li>
-                        <Link to="/shop">swim-equipment</Link>
-                      </li>
-                    </ul>
-                  </div>
+                  {categories.map((category, index) => (
+                    this.renderCollections(category, index, collections)
+                  ))}
                 </div>
               </div>
             </div>
