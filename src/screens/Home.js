eda8039caecaf22a042894c507a573f311783c37
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-03-31 04:20:50 +0100

    banner...

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../assets/styles/screens/home.scss';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export class Home extends Component {
   render() {
     return (
-      <div>
+      <>
         <Header />
-        <h1>Welcome hommmmmmeeeeeee!!!</h1>
+        <div className="homepage">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="banner">
+                <div className="entry-content">
+                  <p>Exiting arrivals</p>
+                  <h1>Get more essentials for less</h1>
+                  <p> Go all out for deals up to 75% off</p>
+                  <Link to="/shop" className="bttn primary">shop now</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <Footer />
-      </div>
+      </>
     )
   }
 }
